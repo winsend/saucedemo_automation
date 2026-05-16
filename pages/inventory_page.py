67d7668cff0d6be4725b7dfd11869c6c02d12a78
Lizogub Vlad <lizogub-vladi@yandex.ru>
2026-05-16 11:23:39 +0300

    Fix webdriver lifecycle management

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,13 +1,8 @@
 from utils.locators import InventoryPageLocators
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from utils.locators import LoginPageLocators
 
 
 class InventoryPage:
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+
     def __init__(self, driver):
         self.driver = driver
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+
 from utils.locators import LoginPageLocators
 
 class LoginPage:
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+
     URL = "https://www.saucedemo.com/"
 
     def __init__(self, driver):
